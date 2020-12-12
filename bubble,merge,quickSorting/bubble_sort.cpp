2f762cc3a87c3af87db7c110f0b706dbf78631d4
Zhaisan Sarsengaliyev <57716933+Zhaiss@users.noreply.github.com>
2020-12-12 21:11:17 +0600

    qw

---
+++ bubble,merge,quickSorting/bubble_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int a[10000000];
+int n;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++)
+            if (a[i] > a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    return 0;
+}
