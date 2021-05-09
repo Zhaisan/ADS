e9927e93814efcd1d193a8079cf845b0590dd6dd
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-05-09 15:26:07 +0600

    basics

---
+++ basicsOF_c++(not algo course)/lab1/1week_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <cmath>
+// A+B
+using namespace std;
+int main () {
+    int a, b;
+    cin >> a >> b;
+    int c=a+b;
+    cout << c;
+    return 0;
+}
---
+++ basicsOF_c++(not algo course)/lab1/1week_4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+// Root
+using namespace std;
+int main () {
+    int x;
+    cin >> x;
+    cout << sqrt (x);
+    return 0; 
+}
